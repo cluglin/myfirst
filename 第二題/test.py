62082cb2c12713f8bfde8e22066ec8e485de4d16
cluglin <37895654+cluglin@users.noreply.github.com>
2021-04-17 08:17:28 +0800

    Update test.py

--- 第二題/test.py
+++ 第二題/test.py
@@ -7,25 +7,23 @@ client = MongoClient('localhost', 27017)
 db = client['Rent_db']
 collection = db['Rent_collection']
 
-# 台北市 前30 https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=0&searchtype=1&region=1
-# 台北市 後30 https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=0&searchtype=1&region=1&firstRow=20&totalRows=12242
-# 新北市 後30 https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=0&searchtype=1&region=3&firstRow=20&totalRows=9025
-#             https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=0&searchtype=1&region=3
 
+# api https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=0&searchtype=1&region=1&firstRow=30&totalRows=12242
+# region只是幌子，改了也不會變更地區，要改地區請找urlJumpIp
 
-def Get_Headers(session):
+def Get_Headers(session):#為了取得CSRF、還有重設瀏覽器cookies預設的urlJumpIp，所以需要先發一個session，CSRF是為了套用API 需要夾在headers裡
     session = session.get(
         'https://rent.591.com.tw/?kind=0&region=3', cookies={"urlJumpIp": "5"})
     soup = BeautifulSoup(session.text, 'html.parser')
-    csfr = soup.find('meta', {'name': 'csrf-token'}).get('content')
+    csrf = soup.find('meta', {'name': 'csrf-token'}).get('content')
     headers = {
-        "X-CSRF-TOKEN": csfr,
+        "X-CSRF-TOKEN": csrf,
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"
     }
     return headers
 
 
-def Get_Info(session, headers):
+def Get_Info(session, headers):#從這個api抓物件(網址、出租者、出租者身分、型態、現況、性別要求)
     cookies = [{"urlJumpIp": "1"}, {"urlJumpIp": "3"}]  # 1=台北，3=新北
     for cookie in cookies:
         url = "https://rent.591.com.tw/home/search/rsList"
